f85b16f96013413972466fda984c5968de14a792
Alan Altindis <altindisa@gmail.com>
2024-10-24 15:30:14 +0600

    qq

--- scrolling.js
+++ scrolling.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
       let currSection = $(this);
       if (currSection.offset().top <= scrollPos + 50 && currSection.offset().top + currSection.height() > scrollPos + 50) {
         let sectionId = currSection.attr("id");
-        $("ul.nav li").removeClass("bgrl");
-        $("ul.nav li a[href='#" + sectionId + "']").parent().addClass("bgrl");
+        $("ul li").removeClass("bgrl");
+        $("ul li a[href='#" + sectionId + "']").parent().addClass("bgrl");
       }
     });
   });
